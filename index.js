736ee8d41fa0efd4e8e3dfeef4f9b4611ec1bc10
Mohammad Rafe <mrrafi5869@gmail.com>
2022-11-25 17:12:10 +0600

    mongodb

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9t60goe.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/', async(req, res) => {
     res.send('used products server is running');
