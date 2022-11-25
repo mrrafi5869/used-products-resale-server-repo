b6cd32a2da6a54528d8f0af94ec2ea168e88280e
Mohammad Rafe <mrrafi5869@gmail.com>
2022-11-25 18:46:11 +0600

    set api

--- index.js
+++ index.js
@@ -10,9 +10,23 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9t60goe.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const carCategories = client.db('usedProductResale').collection('carCategories');
+
+        app.get('/carCategories', async(req, res) => {
+            const query = {};
+            const options = await carCategories.find(query).toArray();
+            res.send(options);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err));
 
 app.get('/', async(req, res) => {
     res.send('used products server is running');
